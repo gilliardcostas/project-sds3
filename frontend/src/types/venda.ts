82a8169c1a01d590badf5d19c6925a0f7707e7c4
Gilliard Santos <37459532+gilliardcostas@users.noreply.github.com>
2021-11-07 22:26:30 -0300

    DataTable integration

--- frontend/src/types/venda.ts
+++ frontend/src/types/venda.ts
@@ -1,3 +1,26 @@
+import { Vendedor } from "./vendedor"
+
+export type Venda = {
+    id: number,
+    visitado: number, 
+    promocoes: number,
+    quantia: number,
+    date: string,
+    vendedor: Vendedor
+}
+
+export type VendasPaginadas = {
+    content?: Venda[],
+    last: boolean,
+    totalElements: number,
+    totalPages: number,
+    size?: number,
+    number: number,
+    first: boolean,
+    numberOfElements?: number
+    empty?: boolean
+}
+
 export type SomaVendas = {
     nomeVendedor: string,
     soma: number
